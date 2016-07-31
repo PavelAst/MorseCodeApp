4de081dc3fa7122486f6299b6d44eadb1eddc58e
PavelAst <nimblekey@gmail.com>
2016-07-31 19:40:19 +0300

    Initial Commit

---
+++ MorseCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MorseCode
+//
+//  Created by Павел Осипенко on 31/07/16.
+//  Copyright © 2016 P.Osipenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
